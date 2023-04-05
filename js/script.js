8996db69b55b2c6d5663fbd043e199415504a7cf
MikeK1639 <126822125+MikeK1639@users.noreply.github.com>
2023-04-04 20:45:59 -0400

    Final changes for the time being

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ function startTimer (){
     if (timeLeft == 0) {
       clearTimeout(timerId);
       console.log("timer check")
-      timer.style.visibility = 'visible'; 
-      startButton.style.visibility = 'hidden';
+      
     } else {
       timer.innerHTML = timeLeft + ' seconds remaining';
       timeLeft--;
@@ -15,52 +14,61 @@ function startTimer (){
   
   var timerId = setInterval(countdown, 1000);
   
-  
 
 }
+function checkanswer(event) { 
+  var choice=event.target.id 
+  if (choice==questions[qindex].correctAnswer){
+    console.log("correct");
+    qindex++ 
+    changequestion();
+  } else{   
+    console.log("incorrect");
+  }
 
+}
 
 
 const startButton = document.querySelector("#start");
 const lose = document.querySelector("#lose");
 lose.addEventListener("click", function () { startButton.style.visibility = 'hidden'; });
-document.querySelector("body1").style.visibility="visible"
-document.querySelector("body2").style.visibility="hidden"
+document.querySelector("body1").classList.remove("hidden")
+document.querySelector("body2").classList.add("hidden")
 var questions = {
   [0]: {
     question: "What is the method to immediately output information using JS",
     choices: ["Console.log", "Control+v", "Alt+f4", "Event listener"],
-    correctAnswer: 0
+    correctAnswer: "A"
   },
 
   [1]: {
     question: "What is the style in which Javascript is written",
     choices: ["mooseCase", "camelCase", "Times New Roman", "kittyCase"],
-    correctAnswer: 1
+    correctAnswer: "B"
   },
 
   [2]: {
     question: "What is a selection of strings used for a loop in JS",
     choices: ["Array", "Variable", "Div", "Repository"],
-    correctAnswer: 0
+    correctAnswer: "A"
   },
 
   [3]: {
     question: "What is the git command for pushing changes to the chosen branch?",
     choices: ["git push", "git pull", "git it started", "get it started in here"],
-    correctAnswer: 0
+    correctAnswer: "A"
   },
 
   [4]: {
     question: "What is the correct HTML tag for inserting a break?",
-    choices: ["breakIt", "<br>", "<h1>", "Insert-Break"],
-    correctAnswer: 1
+    choices: ["breakIt", "br", "h1", "Insert-Break"],
+    correctAnswer: "B"
   },
 
   [5]: {
     questions: "When is it necessary to use the var keyword?",
     choices: ["variable", ".var", "Variable", "var"],
-    correctAnswer: 3
+    correctAnswer: "D"
 
   }
 } 
@@ -72,37 +80,40 @@ const D=document.querySelector("#D")
 
 
 let qindex=0 
-console.log(questions[qindex]);
-qindex++ 
-let q=questions[qindex].question 
-let a=questions[qindex].choices[0] 
-let b=questions[qindex].choices[1]
-let c=questions[qindex].choices[2]
-let d=questions[qindex].choices[3] 
+function changequestion(){
+  quizlength= Object.getOwnPropertyNames(quiz).length 
+  if(questions<quizlength)
+  console.log(questions[qindex]);
+  
+  let q=questions[qindex].question 
+  let a=questions[qindex].choices[0] 
+  let b=questions[qindex].choices[1]
+  let c=questions[qindex].choices[2]
+  let d=questions[qindex].choices[3] 
 
-Q.innerHTML=q
-A.innerHTML=a 
-B.innerHTML=b 
-C.innerHTML=c 
-D.innerHTML=d
+  Q.innerHTML=q
+  A.innerHTML=a 
+  B.innerHTML=b 
+  C.innerHTML=c 
+  D.innerHTML=d
 
-
+}
 
 
 
 
 // Link questions answers to timer 
-// Put answers on page(all buttons??)
 // May need continue button 
 // API for locally stored scores
 // Must keep record!
-// Touch+Tips
 
 
 
-function startbutton() {
-  document.querySelector("body1").style.visibility="hidden"
-document.querySelector("body2").style.visibility="visible"
+
+function startbutton()
+ { changequestion()
+  document.querySelector("body1").classList.add("hidden")
+document.querySelector("body2").classList.remove("hidden")
   console.log("Have Fun!")
   for (let i = 0; i < 6; i++) {
     const element = questions[i].choices;
@@ -112,9 +123,9 @@ document.querySelector("body2").style.visibility="visible"
   startTimer();
 }
 startButton.addEventListener("click", startbutton)  
-A.addEventListener("click", function(){console.log("A")}),
-B.addEventListener("click", function(){console.log("B")})
-C.addEventListener("click", function(){console.log("C")})
-D.addEventListener("click", function(){console.log("D")})
+A.addEventListener("click", checkanswer)
+B.addEventListener("click", checkanswer)
+C.addEventListener("click", checkanswer)
+D.addEventListener("click", checkanswer)
 
 
